d0a26ef801376d78424bd105a94714f55c64538d
Verokeli <ssheng11@my.bcit.ca>
2025-03-04 12:39:40 -0800

    created template for doctorInfo.html file and linked nav bar to redirect to doctorInfo.html

---
+++ scripts/firebaseAPI_TEAM99.js
@@ -0,0 +1,15 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+  apiKey: "AIzaSyCqEk6XsECHj1-4z8kM7jEqCqRQZCWoHw4",
+  authDomain: "bby12-a52bd.firebaseapp.com",
+  projectId: "bby12-a52bd",
+  storageBucket: "bby12-a52bd.firebasestorage.app",
+  messagingSenderId: "475666795315",
+  appId: "1:475666795315:web:602d0f7ad886e1742e8c46"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
