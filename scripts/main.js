f3c82d06849fe12f5f02cc3c6be9a7d3bd3d4d7d
Verokeli <ssheng11@my.bcit.ca>
2025-03-10 14:54:04 -0700

    created a emergency.js file and added Firestore emergency contact data read functionality

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,19 @@ function writeDoctors() {
 
     
 }
+
+function readEmergency(GT2LNyBtUpTutpUeA5NO) {
+    db.collection("emergency").doc(GT2LNyBtUpTutpUeA5NO)      
+        .onSnapshot(GT2LNyBtUpTutpUeA5NODoc => {                                                       
+            console.log("current document data: " + GT2LNyBtUpTutpUeA5NODoc.data());                     
+            document.getElementById("info-goes-here").innerHTML = `
+                <p><strong>Name:</strong> ${GT2LNyBtUpTutpUeA5NODoc.data().Name}</p>
+                <p><strong>Phone Number:</strong> ${GT2LNyBtUpTutpUeA5NODoc.data()["Phone Number"]}</p>
+                <p><strong>Address:</strong> ${GT2LNyBtUpTutpUeA5NODoc.data().Address}</p>
+                <p><strong>Email:</strong> ${GT2LNyBtUpTutpUeA5NODoc.data().Email}</p>
+            `;    
+        }, (error) => {
+            console.log ("Error calling onSnapshot", error);
+        });
+    }
+ readEmergency("GT2LNyBtUpTutpUeA5NO");        
