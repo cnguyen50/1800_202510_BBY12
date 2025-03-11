b089191fea31b127f1aa23a62160539d39911c5b
Leensey <leen1saber@gmail.com>
2025-03-10 22:32:49 -0700

    created medication.js and added firestore data. created card template based on doctor's info to store list of medications

---
+++ scripts/medication.js
@@ -0,0 +1,32 @@
+function displayAllMedications() {
+    let cardTemplate = document.getElementById("medicationCardTemplate"); // Get the card template
+
+    db.collection("medication").get().then(allMedications => {
+        allMedications.forEach(doc => {
+            let medicationData = doc.data();
+            let newCard = cardTemplate.content.cloneNode(true); 
+
+            // Populate card with doctor data
+            newCard.querySelector("#medication-name").innerText = `Name: ${medicationData.name}`;
+            newCard.querySelector("#medication-dose").innerText = `Dosage: ${medicationData.dose}`;
+            newCard.querySelector("#medication-instructions").innerText = `Instructions: ${medicationData.instructions}`;
+
+            // Append card to doctors-list
+            document.getElementById("medication-list").appendChild(newCard);
+        });
+    }).catch(error => {
+        console.error("Error fetching medications:", error);
+    });
+}
+
+// Call the function to display all doctors
+displayAllMedications();
+
+
+
+
+function test() {
+    console.log("testing inside from medication");
+}
+
+test();
