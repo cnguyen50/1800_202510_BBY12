743001f1e4edd5da39db3a375762ada4e4cff4c3
Verokeli <ssheng11@my.bcit.ca>
2025-03-18 10:49:04 -0700

    created save button to update database when emergency contact info is edited

--- scripts/editEmergency.js
+++ scripts/editEmergency.js
@@ -87,3 +87,42 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error("Error fetching emergency contact:", error));
 });
+
+// Function to save the edited doctor information to database
+function saveEmergencyInfo() {
+    let contactName = document.getElementById("contact-name").value;
+    let contactRelationship = document.getElementById("contact-relationship").value;
+    let contactPhone = document.getElementById("contact-phone").value;
+    let contactAddress = document.getElementById("contact-address").value;
+    let contactEmail = document.getElementById("contact-email").value;
+
+    console.log(contactName, contactRelationship, contactPhone, contactAddress, contactEmail);
+
+    // Check if the user is authenticated
+    var user = firebase.auth().currentUser;
+    if (user) {
+        // Get the document reference for the selected doctor
+        var emergencyRef = db.collection("emergency").doc(emergencyID);
+
+        // Update the doctor document with the new data
+        emergencyRef.update({
+            name: contactName,
+            relationship: contactRelationship,
+            Phone: contactPhone,
+            address: contactAddress,
+            email: contactEmail,
+            updatedAt: firebase.firestore.FieldValue.serverTimestamp() 
+        }).then(() => {
+            console.log("Emergency contact information updated successfully!");
+            // window.location.href = "";
+        }).catch(error => {
+            console.error("Error updating emergency contact information: ", error);
+        });
+    } else {
+        console.log("No user is signed in");
+        window.location.href = 'login.html';
+    }
+}
+
+// Add event listener to save button
+document.getElementById("save-button").addEventListener("click", saveEmergencyInfo);
