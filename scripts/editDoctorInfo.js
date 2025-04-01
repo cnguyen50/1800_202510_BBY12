2f0036e4eb62829e815a70d762eb7d8ada8eed8e
cnguyen50 <calvinnguyen50@gmail.com>
2025-03-31 20:11:56 -0700

    fixed user check for doctors info js

--- scripts/editDoctorInfo.js
+++ scripts/editDoctorInfo.js
@@ -6,42 +6,51 @@ document.addEventListener("DOMContentLoaded", function() {
         return;
     }
 
-    // Function to save the edited doctor information to database
-    // Based off of 1800 demo#10 review.js
-    function saveDoctorInfo() {
-        let doctorName = document.getElementById("doctor-name").value;
-        let doctorSpecialization = document.getElementById("doctor-specialization").value;
-        let doctorOffice = document.getElementById("doctor-office").value;
-        let doctorAddress = document.getElementById("doctor-address").value;
-        let doctorEmail = document.getElementById("doctor-email").value;
-
-        console.log(doctorName, doctorSpecialization, doctorOffice, doctorAddress, doctorEmail);
-
-        // Check if the user is authenticated
-        var user = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            // Get the document reference for the selected doctor
-            var doctorRef = db.collection("doctors").doc(doctorID);
 
-            // Update the doctor document with the new data
-            doctorRef.update({
-                name: doctorName,
-                specialization: doctorSpecialization,
-                office: doctorOffice,
-                address: doctorAddress,
-                email: doctorEmail,
-                updatedAt: firebase.firestore.FieldValue.serverTimestamp() 
-            }).then(() => {
-                console.log("Doctor information updated successfully!");
-                window.location.href = "doctorInfo.html";
-            }).catch(error => {
-                console.error("Error updating doctor information: ", error);
-            });
         } else {
             console.log("No user is signed in");
             window.location.href = 'login.html';
         }
-    }
+    })
+
+    // Function to save the edited doctor information to database
+    // Based off of 1800 demo#10 review.js
+    // function saveDoctorInfo() {
+    //     let doctorName = document.getElementById("doctor-name").value;
+    //     let doctorSpecialization = document.getElementById("doctor-specialization").value;
+    //     let doctorOffice = document.getElementById("doctor-office").value;
+    //     let doctorAddress = document.getElementById("doctor-address").value;
+    //     let doctorEmail = document.getElementById("doctor-email").value;
+
+    //     console.log(doctorName, doctorSpecialization, doctorOffice, doctorAddress, doctorEmail);
+
+    //     // Check if the user is authenticated
+    //     var user = firebase.auth().currentUser;
+    //     if (user) {
+    //         // Get the document reference for the selected doctor
+    //         var doctorRef = db.collection("doctors").doc(doctorID);
+
+    //         // Update the doctor document with the new data
+    //         doctorRef.update({
+    //             name: doctorName,
+    //             specialization: doctorSpecialization,
+    //             office: doctorOffice,
+    //             address: doctorAddress,
+    //             email: doctorEmail,
+    //             updatedAt: firebase.firestore.FieldValue.serverTimestamp() 
+    //         }).then(() => {
+    //             console.log("Doctor information updated successfully!");
+    //             window.location.href = "doctorInfo.html";
+    //         }).catch(error => {
+    //             console.error("Error updating doctor information: ", error);
+    //         });
+    //     } else {
+    //         console.log("No user is signed in");
+    //         window.location.href = 'login.html';
+    //     }
+    // }
 
     // Add event listener to save button
     document.getElementById("save-button").addEventListener("click", saveDoctorInfo);
